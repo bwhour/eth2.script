5ad9b682c87ed8dcc0b2e08f99d101a3db914216
bwhour <juvemeen.xio@gmail.com>
2022-04-16 15:36:19 +0800

    update

--- getclients.sh
+++ getclients.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-mkdir eth2clients && cd eth2clients
+mkdir -p eth2clients && pushd ./eth2clients
 //prysm
-mkdir prysm && cd prysm
+mkdir -p prysm && pushd ./prysm
 curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output prysm.sh && chmod +x prysm.sh
+popd
 
 //lighthouse
-curl -C -o lighthouse.tar.gz https://github.com/sigp/lighthouse/releases/download/v2.2.1/lighthouse-v2.2.1-x86_64-unknown-linux-gnu.tar.gz 
-tar -zxvf lighthouse.tar.gz
+wget -c https://github.com/sigp/lighthouse/releases/download/v2.2.1/lighthouse-v2.2.1-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
+
